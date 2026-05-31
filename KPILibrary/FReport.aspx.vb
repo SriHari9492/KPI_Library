ec7b208ae1334347f11549d53e6ebaea4282132d
SriHari <harisriv123@gmail.com>
2026-05-31 15:17:39 +0530

    ABCDEF

--- KPILibrary/FReport.aspx.vb
+++ KPILibrary/FReport.aspx.vb
@@ -3,6 +3,19 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        If Not IsPostBack Then
+            FilterBoxControl.LoadFilters()
+        End If
+    End Sub
+
+    Public Sub ShowReportMessage(fromDate As String, toDate As String)
+        litMessage.Text =
+                $"<div style='padding:10px;border:1px solid green;color:green;'>
+                    Report Generated Successfully!<br/>
+                    <b>Report From:</b> {fromDate}<br/>
+                    <b>Report To:</b> {toDate}
+                  </div>"
     End Sub
 
 End Class
+
